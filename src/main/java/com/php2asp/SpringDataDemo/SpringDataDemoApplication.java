e9bd18b99779e062e0a7e0fa1764c5c4ea87b54c
Izak Smit <oegma2@gmail.com>
2021-11-16 02:30:17 +0100

    Update SpringDataDemoApplication.java
    Adding a logger

--- src/main/java/com/php2asp/SpringDataDemo/SpringDataDemoApplication.java
+++ src/main/java/com/php2asp/SpringDataDemo/SpringDataDemoApplication.java
@@ -1,12 +1,16 @@
 package com.php2asp.SpringDataDemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringDataDemoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SpringDataDemoApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Main application V0.0.1 starting up");
 		SpringApplication.run(SpringDataDemoApplication.class, args);
 	}
 
